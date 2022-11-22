6c3357a3b67f3d1e8e143ff0c548f40eeba29465
molajnef1 <mohamed.neji.lajnef@gmail.com>
2022-11-22 12:19:18 +0100

    fixed the selection of values. only the highest selected value will be considered

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-var button = document.getElementById("Submit");
-var rate_parent = document.getElementById("rate-parent");
-var rating_state = document.getElementById("rating");
-var thanks_state = document.getElementById("thanks_state");
-var desired_rate = document.getElementById("desired-rate");
-var values = Array.from(rate_parent.children);
-var orange = "hsl(25, 97%, 53%)";
-var Dark_blue = "hsl(213, 19%, 18%)";
-var Medium_gray = "hsl(216, 12%, 54%)";
-var id = "selected"
-var result;
+const button = document.getElementById("Submit");
+const rate_parent = document.getElementById("rate-parent");
+const rating_state = document.getElementById("rating");
+const thanks_state = document.getElementById("thanks_state");
+const desired_rate = document.getElementById("desired-rate");
+const values = Array.from(rate_parent.children);
+const orange = "hsl(25, 97%, 53%)";
+const Dark_blue = "hsl(213, 19%, 18%)";
+const Medium_gray = "hsl(216, 12%, 54%)";
+const selected = "selected"
+let result;
+let selection_counter = 0;
 
 button.addEventListener("click", function(){
     rating_state.style.display = "none";
     thanks_state.style.display = "block";
-    desired_rate.innerText = result;
+    
+    get_the_selected_rate();
 });
 
-
-
+function get_the_selected_rate() {
     values.forEach(rate => {
-        console.log(rate.className)
-        rate.addEventListener("click", function(){
+        if(rate.className == selected){
+            desired_rate.innerText = rate.innerText;
+        }
+    });
+}
+
+
+values.forEach(rate => {
+    console.log(rate.className)
+    rate.addEventListener("click", function(){
            /* result = rate.textContent;
             rate.style.background = orange;
             rate.className += id;*/
 
-            switch (rate.className) {
-                case "selected":
-                    rate.style.background = Dark_blue
-                    rate.className = ""
-                    result = "";
-                    break;
-                default:
-                    result = rate.textContent;
-                    rate.style.background = Medium_gray;
-                    rate.className += id;
-                    break;
+    switch (rate.className) {
+        case "selected":
+            rate.style.background = Dark_blue
+            rate.className = ""
+            desired_rate.innerText = ""
+            break;
+        default:
+            rate.style.background = Medium_gray;
+            rate.className += selected;
+            break;
             }
             });
         });
